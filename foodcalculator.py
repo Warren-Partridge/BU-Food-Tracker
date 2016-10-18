aaf2286d7de458ab814cdce204b959d3c239b607
Warren-Partridge <maps@perdix.org>
2016-10-17 21:53:53 -0400

    Implements use of the Python time module
    Implements some small demoes of getting the current time via the Python
    time module.
    
    Sets up a dictionary in foodcalculator.py that maps days of the week to
    ints 0-6.
    
    Also fixes a silly bug with importing the foodcalculator module (it was
    previously importing foodcalculators… I must have been hungry last
    night)

--- foodcalculator.py
+++ foodcalculator.py
@@ -1,2 +1,16 @@
-def getMealStatus(mealsPerWeek, dayOfWeek):
+import time;
+WEEKDAY_DICT = {"Saturday": 0,
+                "Sunday": 1,
+                "Monday": 2,
+                "Tuesday": 3,
+                "Wednesday": 4,
+                "Thursday": 5,
+                "Friday": 6};
+# This dictionary is indexed from Saturday because Friday is the day that
+# meal swipes are reset. This way, getting distance to Friday is easier.
+
+def getMealStatus(mealsPerWeek):
+    weekday = time.strftime("%A");
+    weekdayNum = WEEKDAY_DICT[weekday];
+    print(weekdayNum);
     return mealsPerWeek;
--- run.py
+++ run.py
@@ -1,4 +1,5 @@
-from foodcalculators import *;
+from foodcalculator import *;
+import time;
 
 print("Hello! Which of the following meal plans are you currently using?");
 print("1. Unlimited Plan\n2. 14-Plus Plan\n3. 9-Plus Plan\n4. 330 Plan\n" +\
@@ -13,8 +14,9 @@ print(userPlan);
 # TODO: Implement other plans. For now I will start with 9-Plus
 
 print("The 9-Plus Plan has 9 meals per week and 415 dining dollars per semester.");
-print("The current date is " + date + ", which is a " + dayofweek);
+print("The current date is " + time.strftime("%m/%d/%Y") + ", which is a " +\
+      time.strftime("%A") + ".");
 # if date is between Sept 6 - Dec 12, it's fall semester
 # if date is between Jan 19 - May 12, it's spring semester
 
-print(getMealStatus(12, 12));
+print(getMealStatus(9)); # Testing for the 9-Plus plan
